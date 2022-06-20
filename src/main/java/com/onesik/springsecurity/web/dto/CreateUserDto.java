a8feec72999fe03d76f97674fb903072c931e2fe
ones1kk <ones1k95@gmail.com>
2022-06-21 01:03:28 +0900

    ♻️ Refactor : CreateUserDto > ToEntity()

--- src/main/java/com/onesik/springsecurity/web/dto/CreateUserDto.java
+++ src/main/java/com/onesik/springsecurity/web/dto/CreateUserDto.java
@@ -1,5 +1,6 @@
 package com.onesik.springsecurity.web.dto;
 
+import com.onesik.springsecurity.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,4 +15,12 @@ public class CreateUserDto {
     private String username;
     private String birthDate;
     private String phoneNo;
+
+    public User toEntity() {
+        return User.builder()
+                .username(this.username)
+                .birthDate(this.birthDate)
+                .phoneNo(this.phoneNo)
+                .build();
+    }
 }
