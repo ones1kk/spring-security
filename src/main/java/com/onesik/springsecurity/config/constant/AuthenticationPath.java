add95e1bb0a94f65ea970b630a2d1cd1a701b0ca
ones1kk <ones1k95@gmail.com>
2022-06-22 01:19:19 +0900

    ✨ Add : New path

--- src/main/java/com/onesik/springsecurity/config/constant/AuthenticationPath.java
+++ src/main/java/com/onesik/springsecurity/config/constant/AuthenticationPath.java
@@ -10,7 +10,11 @@ public enum AuthenticationPath {
     FIRST_LOGIN_PAGE("/login/first"),
     FIRST_LOGIN_API("/apis/login/first"),
     SECOND_LOGIN_PAGE("/login/second"),
-    SECOND_LOGIN_API("/apis/login/second");
+    SECOND_LOGIN_API("/apis/login/second"),
+
+    HOME_PAGE("/"),
+
+    LOGOUT_API("/apis/logout");
 
     private final String path;
 }
