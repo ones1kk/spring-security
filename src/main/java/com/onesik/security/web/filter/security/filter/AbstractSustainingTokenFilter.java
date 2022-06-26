0c5b6dd14ed6f2601bcc29d8a924d7dbe8ef7130
ones1kk <ones1k95@gmail.com>
2022-06-26 23:47:57 +0900

    🐛 Fix : AbstractSustainingTokenFilter

--- src/main/java/com/onesik/security/web/filter/security/filter/AbstractSustainingTokenFilter.java
+++ src/main/java/com/onesik/security/web/filter/security/filter/AbstractSustainingTokenFilter.java
@@ -1,6 +1,6 @@
 package com.onesik.security.web.filter.security.filter;
 
-import com.onesik.security.web.jwt.AbstractJwtTokenProvider;
+import com.onesik.security.web.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -12,21 +12,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static com.onesik.security.web.jwt.AbstractJwtTokenProvider.X_AUTH_TOKEN;
+import static com.onesik.security.web.jwt.JwtTokenProvider.X_AUTH_TOKEN;
 
 @RequiredArgsConstructor
 public abstract class AbstractSustainingTokenFilter extends OncePerRequestFilter {
 
-    private final AbstractJwtTokenProvider<Authentication> jwtTokenProvider;
+    private final JwtTokenProvider<Authentication> jwtTokenProvider;
 
     protected Authentication get(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         Cookie[] cookies = request.getCookies();
         if (cookies == null || cookies.length == 0) {
-            filterChain.doFilter(request, response);
             return null;
         }
 
         String jwtToken = jwtTokenProvider.resolveToken(request, X_AUTH_TOKEN);
+        if(jwtToken == null) return null;
         return jwtTokenProvider.getAuthentication(jwtToken);
     }
 
