c9c79b45d46953820ea8af3c1013e92b64d87fbe
ones1kk <ones1k95@gmail.com>
2022-06-29 00:44:34 +0900

    ✨ Update : ViewController > User from jwtToken

--- src/main/java/com/onesik/security/web/controller/ViewController.java
+++ src/main/java/com/onesik/security/web/controller/ViewController.java
@@ -1,13 +1,36 @@
 package com.onesik.security.web.controller;
 
+import com.onesik.security.domain.User;
+import com.onesik.security.service.UserService;
+import com.onesik.security.web.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+import static com.onesik.security.web.jwt.JwtTokenProvider.*;
+
 @Controller
+@RequiredArgsConstructor
 public class ViewController {
 
+    private final UserService userService;
+    private final JwtTokenProvider<Authentication> jwtTokenProvider;
+
     @GetMapping("/home")
-    String getHomePage() {
+    String getHomePage(HttpServletRequest request, Model model) {
+        Authentication authentication = jwtTokenProvider.resolveAndGet(request, X_AUTH_TOKEN);
+
+        User user = (User) authentication.getPrincipal();
+        Long userId = user.getId();
+
+        User findUser = userService.findById(userId);
+
+        model.addAttribute("user", findUser);
+
         return "home";
     }
 
