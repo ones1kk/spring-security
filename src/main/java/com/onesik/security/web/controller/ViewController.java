62f433db2c28a5502b4a53c4f32b2fbc1d485019
ones1kk <ones1k95@gmail.com>
2022-07-01 00:27:44 +0900

    ✨ Add : @Model.addAttribute > errorMessage

--- src/main/java/com/onesik/security/web/controller/ViewController.java
+++ src/main/java/com/onesik/security/web/controller/ViewController.java
@@ -2,15 +2,20 @@ package com.onesik.security.web.controller;
 
 import com.onesik.security.domain.User;
 import com.onesik.security.service.UserService;
+import com.onesik.security.web.filter.security.handler.FirstAuthenticationFailureHandler;
 import com.onesik.security.web.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.codehaus.groovy.util.StringUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static com.onesik.security.web.filter.security.handler.FirstAuthenticationFailureHandler.*;
 import static com.onesik.security.web.jwt.JwtTokenProvider.X_AUTH_TOKEN;
 
 @Controller
@@ -20,7 +25,12 @@ public class ViewController {
     private final UserService userService;
     private final JwtTokenProvider<Authentication> jwtTokenProvider;
 
-    private final JwtTokenProvider<String> exception;
+    @ModelAttribute
+    public void getErrorMessage(Model model, @ModelAttribute(ERROR_MESSAGE) String errorMessage) {
+        if(StringUtils.hasText(errorMessage)) {
+            model.addAttribute("errorMessage", errorMessage);
+        }
+    }
 
     @GetMapping("/home")
     String getHomePage(HttpServletRequest request, Model model) {
@@ -37,7 +47,7 @@ public class ViewController {
     }
 
     @GetMapping("/login/first")
-    String getFirstLoginPage(HttpServletRequest request, Model model) {
+    String getFirstLoginPage() {
         return "login/first";
     }
 
